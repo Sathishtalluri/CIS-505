dc04922d51475b3d972bdc314e5b6609f2076c62
Sathish <Satalluri@my365.bellevue.edu>
2021-09-11 04:57:34 -0500

    module_2 TalluriCombined.jar commit by Sateesh on 9/11/2021

---
+++ module_2/JarPracticeFiles/TalluriJarOne.java
@@ -0,0 +1,34 @@
+
+/**
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+   Comprehensive Version (12th ed.). Pearson Education, Inc.
+   Modified by R. Sateesh 2021
+   Additional modifications by T. Sathish 2021
+*  Payne, D. (2021). CSD 405 Intermediate Java Programming. Bellevue University.
+   Modified by A. Sateesh 2021
+
+ */
+/**
+ * Class name Declaration with Name TalluriJarOne
+ */
+public class TalluriJarOne
+{
+/**
+ * Declaration of main method in TalluriJarOne class
+ * This first executed method
+ */
+    public static void main(String[] args)
+    {
+    /**
+     * Declaring the two variables named as firstName and lastName in camel case with values
+     */
+    String firstName="Sathish";
+    String lastName="Talluri";
+    /**
+     * command to print declared variables values in cosole
+     */
+    System.out.println("Hello world from "+firstName+" "+lastName);
+
+    }//closing Tag of Main methos
+
+}//closing tag of the TalluriHelloWorld class 
---
+++ module_2/JarPracticeFiles/TalluriJarTwo.java
@@ -0,0 +1,15 @@
+/**
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+   Comprehensive Version (12th ed.). Pearson Education, Inc.
+   Modified by R. Sateesh 2021
+   Additional modifications by T. Sathish 2021
+*  Payne, D. (2021). CSD 405 Intermediate Java Programming. Bellevue University.
+   Modified by A. Sateesh 2021
+
+ */
+/**
+ * Class name Declaration with Name TalluriJarTwo
+ */
+public class TalluriJarTwo {
+    
+}
